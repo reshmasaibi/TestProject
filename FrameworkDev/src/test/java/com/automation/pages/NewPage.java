f3867a3383a2fd704d3155b68d7e2cf334414cbb
ssaibi@tavisca.com <ssaibi@tavisca.com>
2019-02-01 13:51:27 +0530

    new class added

---
+++ FrameworkDev/src/test/java/com/automation/pages/NewPage.java
@@ -0,0 +1,12 @@
+package com.automation.pages;
+
+import org.testng.annotations.Test;
+
+public class NewPage {
+
+	@Test
+	public void tpTestGit()
+	{
+		System.out.println("Inside new class for git");
+	}
+}
